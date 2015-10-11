33076ea9148f477f21961925da31166c37ec9518
xke <key2xanadu+github@gmail.com>
2015-10-11 13:08:09 -0700

    required features

---
+++ Twitter/MenuTransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  MenuTransitionManager.swift
+//  Twitter
+//
+//  Created by Xian on 10/10/15.
+//  Copyright © 2015 swifterlabs. All rights reserved.
+//
+
+import UIKit
+
+class MenuTransitionManager: UIViewControllerTransitioningDelegate {
+
+}
